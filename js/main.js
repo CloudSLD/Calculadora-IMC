ea1e32ed7946df8bb9e53a2a1f9be86f86a208df
CloudSLD <cjbrvii@gmail.com>
2024-01-12 11:16:04 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+import {Modal} from './modal.js'
+import {AlertError} from './alert.js'
+import {IMC, notANumber} from './utils.js'
+
+
+const form = document.querySelector('form')
+const inputWeight =  document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
+
+
+
+
+form.onsubmit = function(event) {
+  event.preventDefault() 
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  const showAlertError = notANumber(weight) || notANumber(height)
+
+  if (showAlertError) {
+    AlertError.open()
+    return
+  }
+
+  
+
+  const result = IMC(weight, height)
+  const message = `Seu IMC é de ${result}`
+
+
+  Modal.message.innerText = message
+  Modal.open()
+  
+}
+
+//Event Error
+inputWeight.oninput  = () => AlertError.close()
+inputWeight.oninput = () =>  AlertError.close()
+
+
